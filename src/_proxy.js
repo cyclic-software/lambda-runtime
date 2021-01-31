07befa7b2bf122f926293bd7a62ee52244c15efa
Kam Lasater <kam@cyclic.sh>
2021-01-30 21:32:38 -0500

    Add more of the request params/headers/etc

--- src/_proxy.js
+++ src/_proxy.js
@@ -2,21 +2,24 @@ const axios = require('axios').default;
 
 require('./index')
 
-
-
-
 exports.handler = async function(event, context) {
   console.log("calling the server")
   console.log(`event: ${JSON.stringify(event)}`)
   console.log(`context: ${JSON.stringify(context)}`)
   return (
-    // await axios.(`http://localhost:3000`, {headers: {"Content-Type": "application/json"}})
     await axios({
-      method: event.method,
-      url: event.path,
       baseURL: "http://localhost:3000/",
+      body: event.body,
       headers: event.headers,
-      port: 3000
+      method: event.method,
+      params: event.params,
+      url: event.path,
     })
   ).data
 }
+
+// exports.handler = async function(event, context) {
+//   console.log(`Event recieved: ${JSON.stringify(event)}`)
+//   console.log(`process.env: ${JSON.stringify(process.env)}`)
+//   return process.env
+// }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 3000
 
 app.all('*', async (req, res) => {
-  console.log(`express called with ${req.method}:${req.path}`)
+  console.log(`express called with ${req.method}:${req.path} ... ${JSON.stringify(req.headers)}`)
   res.json({
     method: req.method,
     path: req.path,
@@ -15,18 +15,3 @@ app.all('*', async (req, res) => {
 app.listen(PORT, ()=>{
   console.log('server started')
 })
-
-// console.log("hello world")
-// setTimeout(()=>{
-//   console.log("Done")
-// },5000)
-
-// exports.handler = async function(event, context) {
-//   console.log(`Event recieved: ${JSON.stringify(event)}`)
-//   console.log(`process.env: ${JSON.stringify(process.env)}`)
-//   return process.env
-// }
-
-// exports.handler({name: "event.name"},{"context":"this is the context"})
-
-// AWS_LAMBDA_RUNTIME_API
