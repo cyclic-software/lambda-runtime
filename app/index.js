33c22658c042e47c1d68519abb42f195efc13885
Kam Lasater <kam@cyclic.sh>
2021-02-11 05:58:34 -0500

    Added error route and body-parser to sample app

--- app/index.js
+++ app/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const PORT = 3000
+
+// Accept both form and json encoded bodies
+app.use(bodyParser.urlencoded({extended: false})); // application/x-www-form-urlencoded
+app.use(bodyParser.json()); // application/json
+
+app.all('/error', async (req,res) => {
+  console.log(`express called with ${req.method}:${req.path} ... ${JSON.stringify(req.headers)}`)
+
+  res.statusCode = req.body.statusCode || 404
+  res.json({
+    method: req.method,
+    path: req.path,
+    headers: req.headers,
+    message: "Triggering 404 status response",
+  })
+})
 
 app.all('*', async (req, res) => {
   console.log(`express called with ${req.method}:${req.path} ... ${JSON.stringify(req.headers)}`)
--- src/_proxy.js
+++ src/_proxy.js
@@ -17,3 +17,22 @@ exports.handler = async function(event, context) {
     })
   ).data
 }
+
+
+
+// axios.get('/api/xyz/abcd')
+//   .catch(function (error) {
+//     if (error.response) {
+//       // Request made and server responded
+//       console.log(error.response.data);
+//       console.log(error.response.status);
+//       console.log(error.response.headers);
+//     } else if (error.request) {
+//       // The request was made but no response was received
+//       console.log(error.request);
+//     } else {
+//       // Something happened in setting up the request that triggered an Error
+//       console.log('Error', error.message);
+//     }
+
+//   });
